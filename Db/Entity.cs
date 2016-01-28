d1aaaf7ab44159e50b4a2934c45feb30db3253f3
DasJott <das.jott@gmail.com>
2016-01-28 14:38:43 +0100

    Some empty lines

--- Db/Entity.cs
+++ Db/Entity.cs
@@ -5,12 +5,12 @@ namespace DasJott.Database
   public abstract class Entity
   {
     public int ID { get; set; }
-    
+
     /// is automaticly set
     public DateTime Created { get; set; }
     /// is automaticly set
     public DateTime Updated { get; set; }
-    public bool Deleted { get; set; }    
+    public bool Deleted { get; set; }
     internal virtual void OnCreate() {}
     internal virtual void OnUpdate() {}
   }
