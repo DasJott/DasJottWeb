20bd669cc6dd8598ea6c7b5ff525fbfa016c6edc
DasJott <das.jott@gmail.com>
2016-01-31 00:12:06 +0100

    Put some stuff from common to start

--- Sections/Common/Content/js/main.js
+++ Sections/Common/Content/js/main.js
@@ -5,8 +5,6 @@ $(function() {
 
   setMenuSelection();
 
-  // smooth page changes
-  setupAsyncSectionLoading(100, 300);
   logger.debug("main.js loaded");
 });
 
@@ -16,45 +14,6 @@ window.loadSpinner = new JottLoadSpinner();
 window.logger = new Utils.Logger();
 
 $('.chosen-select').chosen();
-
-// smooth page changes from main menu
-function setupAsyncSectionLoading(fadeOut, fadeIn) {
-  if (!fadeOut)  { fadeOut = 300; }
-  if (!fadeIn) { fadeIn = fadeOut; }
-
-  var $content = $('#bodyContent');
-
-  // load pages by ajax
-  $('#mainMenu > li').click(function() {
-    var $elem = $(this);
-    var $prev = $elem.parent().find("li.active");
-    if (!$elem.hasClass('active')) {
-      $prev.removeClass('active');
-      $elem.addClass('active');
-      $content.fadeOut(fadeOut, function() {
-        var $a = $elem.find('a');
-        var url = $a.attr("href")
-        var title = $a.text();
-        logger.debug("menu navigation: " + url);
-        if (url == '/') {
-          document.location.pathname = url;
-        } else {
-          logger.debug("Ajax loading");
-          $content.load(url, '', function(responseText, textStatus, jqXHR) {
-            logger.debug("loading complete: " + textStatus);
-            if (textStatus === "error") {
-              var win = window.open("", "Error");
-              win.document.write(responseText);
-            }
-            document.title = title + " - " + document.title;
-            $content.fadeIn(fadeIn);
-          })
-        }
-      });
-    }
-    return false;
-  });
-}
 
 // make sure, the correct menu item is selected
 function setMenuSelection() {
--- Sections/Start/Content/js/Index.js
+++ Sections/Start/Content/js/Index.js
@@ -1,7 +1,69 @@
 $(function() {
-  $('#bodyContent').load('/News/Index', '', function() {
-    $('#bodyContent').fadeIn();
-  });
+  // smooth page changes
+  setupAsyncSectionLoading(100, 300);
+
+  // load start page
+  loadFirstPage(500);
+
+  /////////////////////////////////////////////////////////////////////////////
+
+  // load first page
+  function loadFirstPage(fadeIn)
+  {
+    var $content = $('#bodyContent');
+    var $a = $('#mainMenu > li > a').first();
+    var url = $a.attr("href")
+    var title = $a.text();
+    loadContent($content, url, title, 100, fadeIn);
+  }
+
+  // smooth page changes from main menu
+  function setupAsyncSectionLoading(fadeOut, fadeIn) {
+    var $content = $('#bodyContent');
+
+    // load pages by ajax
+    $('#mainMenu > li').click(function() {
+      $('#bodyContentError').hide();
+
+      var $elem = $(this);
+      var $prev = $elem.parent().find("li.active");
+      if (!$elem.hasClass('active')) {
+        $prev.removeClass('active');
+        $elem.addClass('active');
+        $content.fadeOut(fadeOut, function() {
+          var $a = $elem.find('a');
+          var url = $a.attr("href")
+          var title = $a.text();
+          logger.debug("menu navigation: " + url);
+          if (url == '/') {
+            document.location.pathname = url;
+          } else {
+            loadContent($content, url, title, fadeOut, fadeIn);
+          }
+        });
+      }
+      return false;
+    });
+  }
+
+  function loadContent($content, url, title, fadeIn) {
+    if (!fadeIn) { fadeIn = 300; }
+
+    logger.debug("Ajax loading");
+
+    $content.load(url, '', function(responseText, textStatus, jqXHR) {
+      logger.debug("loading complete: " + textStatus);
+      if (textStatus === "error") {
+        var win = window.open("", "Error");
+        win.document.write(responseText);
+        $('#bodyContentError').text("An error occured").fadeIn();
+      } else {
+        document.title = title + " - " + document.title;
+        $content.fadeIn(fadeIn);
+      }
+    })
+
+  }
 
   logger.debug("Start/Index loaded");
 });
